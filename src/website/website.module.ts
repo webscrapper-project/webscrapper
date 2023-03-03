eeca13229ff3fac7e4dc2fee9654ad0f2d1de6c4
Emil Məmmədov <emill.memmedovv@gmail.com>
2023-03-03 13:03:46 +0400

    replace readme

--- src/website/website.module.ts
+++ src/website/website.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import {WebsiteController} from "./controller/website.controller";
 import {WebsiteService} from "./service/website.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {JobSchema} from "../job/model/job.schema";
@@ -14,7 +13,6 @@ import {WebsiteDetailSchema} from "./model/website-detail.schema";
       },
     ]),
   ],
-  controllers: [WebsiteController],
   providers: [WebsiteService],
   exports: [WebsiteService]
 })
